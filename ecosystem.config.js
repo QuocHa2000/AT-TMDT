7cc9224905482a413951870adf60ff0be31a1b44
Ha Quoc <havuongquoc2000@gmail.com>
2021-10-23 11:07:31 +0700

    init commit

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+const REPO = "git@github.com:QuocHa2000/AT-TMDT.git";
+module.exports = {
+  apps: [
+    {
+      name: "AT_TMDTs",
+      script: "node ./dist/app.js",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "18.163.166.211",
+      ref: "origin/master",
+      repo: REPO,
+      ssh_options: "StrictHostKeyChecking=no",
+      path: "/root/home/ubuntu/mobile-server/",
+      "post-deploy":
+        "npm install && npm run build && pm2 startOrRestart /home/ubuntu/mobile-server/ecosystem.config.js && pm2 save",
+    },
+  },
+};
---
+++ src/app.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const multer = require("multer");
+
+const app = express();
+
+var options = {
+  useNewUrlParser: true,
+  autoIndex: true,
+  useUnifiedTopology: true,
+};
+console.log(process.env.DATABASE_URI);
+mongoose.connect(process.env.DATABASE_URI, options);
+
+const inforSchema = new mongoose.Schema({
+  user_name: String,
+  password: String,
+  front_certificate: String,
+  back_certificate: String,
+  phone: String,
+  email: String,
+});
+
+const inforModel = mongoose.model("information", inforSchema);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../asset"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, "/images" + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/", upload.array("certificate", 2), function (req, res, next) {
+  const {
+    user_name,
+    password,
+    front_certificate,
+    back_certificate,
+    phone,
+    email,
+  } = req.body;
+  inforModel.create({
+    user_name,
+    password,
+    front_certificate: req.files[0].filename,
+    back_certificate: req.files[1].filename,
+    phone: phone.toString(),
+    email,
+  });
+  res.json({ message: "KYC successfully !" });
+});
+
+app.get("/", async function (req, res, next) {
+  const data = await inforModel.find({});
+  res.json({ data });
+});
+
+app.get("/:id", async function (req, res, next) {
+  const { id } = req.params;
+  const data = await inforModel.findById(id);
+  res.json({ data });
+});
+
+app.listen("3000", () => console.log("App is on 3000"));
